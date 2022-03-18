bd700ed8455c813ce18a41ee7f0cd37eb36eac4c
Lil_Tim <99663083+LilTim0@users.noreply.github.com>
2022-03-18 09:40:47 +0100

    2022.03.18-09:40

--- eezCrypt.py
+++ eezCrypt.py
@@ -3,6 +3,7 @@ import random
 
 print('------------------------\n-  eezCrypt Algorythm  -\n------------------------')
 
+BITS = 256
 seed = 0
 fileToEncrypt = input ('Name of the file to encrypt : ')
 mode = 'r'
@@ -12,14 +13,11 @@ mode = 'r'
 def generateKey(bits):
 	key = ''
 	chars = '0123456789abcdef'
-	seed = int(bits / 8 * 7)
-	for i in range(bits):
+	for i in range(bits - 4):
 		index = random.randint(0,len(chars)-1)
 		key += chars[index]
 
-	set = (key, seed)
-
-	return set
+	return key
 
 # ----------------------------------------------------------------------------------------
 
@@ -27,17 +25,17 @@ def getValues(string):
 	values = [0] * len(string)
 	for i in range(len(string)):
 		if(string[i]=='a'):
-			values[i] = '10'
+			values[i] = 10
 		elif(string[i]=='b'):
-			values[i] = '11'
+			values[i] = 11
 		elif(string[i]=='c'):
-			values[i] = '12'
+			values[i] = 12
 		elif(string[i]=='d'):
-			values[i] = '13'
+			values[i] = 13
 		elif(string[i]=='e'):
-			values[i] = '14'
+			values[i] = 14
 		elif(string[i]=='f'):
-			values[i] = '15'
+			values[i] = 15
 		else:
 			values[i] = string[i]
 
@@ -54,52 +52,119 @@ def compareValues(len_values, j):
 	
 # ----------------------------------------------------------------------------------------
 
-print('\n\nRepo : ' + os.getcwd())
+def getPassages():
+	return random.randint(0,9)
+	
+# ----------------------------------------------------------------------------------------
 
+def getAddMode():
+	return random.randint(0, 2)
+	
+# ----------------------------------------------------------------------------------------
+
+def getFrequence():
+	return random.randint(1, 3)
+	
+# ----------------------------------------------------------------------------------------
+
+def getOffset():
+	return random.randint(0, 9)
+
+# ----------------------------------------------------------------------------------------
+
+def decimalToBinary(num):
+  return bin(num).replace("0b", "")
+
+# ----------------------------------------------------------------------------------------
+	
+print('\n\nRepo : ' + os.getcwd() + '\n\n')
+
+# Check if the file exists
 if os.path.exists(fileToEncrypt):
+	# Choose a mode
 	encryptOrNot = input('Would you like to encrypt or to decrypt this file (e/d)?')
 
+	# Load the content of the file
+	with open(fileToEncrypt, mode) as file :
+		content = file.read()
+		
+	# Convert the content in hexa
+	bin = content.encode('utf-8').hex()
+
 	if encryptOrNot == 'e':
-		with open(fileToEncrypt, mode) as file :
-			bin = file.read().encode('utf-8').hex()
-			print('\n\n')
-			# print(bin)
-	
-		datas = generateKey(256)
+		key = generateKey(BITS)
+		all = [0] * BITS
+		
+		for i in range(BITS - 4):
+			all[i] = key[i]
+			
+		all[-4] = getPassages()
+		all[-2] = getFrequence()
+		
+		key = all[:252]
 			
 		unc_values = getValues(bin)
 		for i in range(len(unc_values)):
 			print(unc_values[i])
-		key_values = getValues(datas[0])
+		key_values = getValues(key)
 	
-		result_values = [0] * (len(unc_values)+1)
-			
+		result_values = [0] * (len(unc_values))
+		result_lengths = [0] * len(result_values)
+		result_offsets = [0] * len(result_values)
+		result_add_mode = [0] * len(result_values)
+		
+		temp = [0] * (len(unc_values))
 		for i in range(len(unc_values)):
-			j = i
-			while j >= len(key_values):
-				j -= len(key_values)
-			result_values[i] = str(
-	int(unc_values[i]) + int(key_values[j]))
-			print(result_values[i])
-			result_lengths = [0] * len(result_values)
-			for i in range(len(result_values)):
+			temp[i] = int(unc_values[i])
+
+		k = 0
+		while k <= all[-4]:
+			for i in range(0, len(temp), all[-2]):
+				j = i
+				while j >= len(key_values):
+					j -= len(key_values)
+				tempOffset = getOffset()
+				tempAddMode = getAddMode()
+				
+				temp[i] += int(tempOffset)
+				
+				if tempAddMode == 0: result_values[i] = int(temp[i])
+				elif tempAddMode == 1: result_values[i] = int(str(temp[i]) + str(key_values[j]))
+				elif tempAddMode == 2: result_values[i] = int(temp[i]) + int(key_values[j])
+					
 				result_lengths[i] = len(str(result_values[i]))
-				print(result_lengths[i])
+				result_offsets[i] = tempOffset
+				result_add_mode[i] = tempAddMode
+				
+				temp[i] = result_values[i]
+					
+			k += all[-2]
 
-# ----------------------------------------------------
+		for i in range(len(result_offsets)):
+			all[-1] += result_offsets[i]
+			all[-3] += result_add_mode[i]
 
+		final = [0] * len(result_values)
+		for i in range(len(result_values)): 
+			final[i] = decimalToBinary(result_values[i])
+		
 		mode = 'w'
 		with open(fileToEncrypt, mode) as file:
 			for i in range(len(result_values)):
 				file.write(str(result_values[i]))
 	
-		with open('key.txt', 'x') as file:
-			file.write(datas[0])
+		with open('key', 'x') as file:
+			temp = ''
+			for i in range(len(all)):
+				temp += str(all[i])
+			file.write(temp)
+
+# ----------------------------------------------------
 
 	elif encryptOrNot == 'd':
-		# Here
+		print(bin)
 
-		else:
-			print('Invalid value')
+	else:
+		print('Invalid value')
 			
 else: print("This file doesn't exist")
